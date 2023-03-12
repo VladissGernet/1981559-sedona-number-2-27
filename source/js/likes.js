f9a537ff5b048a9da7f9698f13a7506b80e0a955
Vlad <skeimorr@gmail.com>
2023-03-12 18:38:08 +0400

    for of cycle added

--- source/js/likes.js
+++ source/js/likes.js
@@ -1,15 +1,17 @@
-let cardLike = document.querySelector('.card-like');
+let cardsLike = document.querySelectorAll('.card-like');
 let cardLikeValue = document.querySelector('.card-like__value');
 let icon = document.querySelector('.card-like__xlink')
 
-cardLike.onclick = function () {
-  if (cardLike.classList.contains('card-like--added')) {
-    cardLikeValue.textContent--;
-    cardLike.classList.remove('card-like--added');
-    icon.setAttribute("xlink:href", "img/sprite.svg#like-icon")
-  } else {
-    cardLikeValue.textContent++;
-    cardLike.classList.add('card-like--added');
-    icon.setAttribute("xlink:href", "img/sprite.svg#like-icon-added")
+for (let cardLike of cardsLike) {
+  cardLike.onclick = function () {
+    if (cardLike.classList.contains('card-like--added')) {
+      cardLikeValue.textContent--;
+      cardLike.classList.remove('card-like--added');
+      icon.setAttribute("xlink:href", "img/sprite.svg#like-icon")
+    } else {
+      cardLikeValue.textContent++;
+      cardLike.classList.add('card-like--added');
+      icon.setAttribute("xlink:href", "img/sprite.svg#like-icon-added")
+    }
   }
 }
