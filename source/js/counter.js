c3ff57eac905c8ea2e313d4d54555116678c23e4
Vlad <skeimorr@gmail.com>
2023-03-14 18:13:25 +0400

    counter fixed

--- source/js/counter.js
+++ source/js/counter.js
@@ -2,6 +2,6 @@ let commentField = document.querySelector('.feedback-form__user-text');
 let charCounter = document.querySelector('.feedback-form__char-counter');
 
 commentField.oninput = function () {
-  charCounter.textContent = charCounter.value.length;
+  charCounter.textContent = commentField.value.length;
 };
 
