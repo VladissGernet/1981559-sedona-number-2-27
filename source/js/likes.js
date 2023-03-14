922fa6a117f6a24c7433abd647c994d0cd36ca68
Vlad <skeimorr@gmail.com>
2023-03-14 18:27:16 +0400

    like fixing is started

--- source/js/likes.js
+++ source/js/likes.js
@@ -1,17 +1,8 @@
-let cardsLike = document.querySelectorAll('.card-like');
+let cardLikes = document.querySelectorAll('.card-like');
 let cardLikeValue = document.querySelector('.card-like__value');
-let icon = document.querySelector('.card-like__xlink')
 
-for (let cardLike of cardsLike) {
+for (let cardLike of cardLikes) {
   cardLike.onclick = function () {
-    if (cardLike.classList.contains('card-like--added')) {
-      cardLikeValue.textContent--;
-      cardLike.classList.remove('card-like--added');
-      icon.setAttribute("xlink:href", "img/sprite.svg#like-icon")
-    } else {
-      cardLikeValue.textContent++;
-      cardLike.classList.add('card-like--added');
-      icon.setAttribute("xlink:href", "img/sprite.svg#like-icon-added")
-    }
+    cardLike.classList.toggle('card-like--added');
   }
 }
