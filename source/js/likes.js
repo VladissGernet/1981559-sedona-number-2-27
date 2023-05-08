78b56e62e3b67d4629b6f33389446da91e5fa68a
Vlad <skeimorr@gmail.com>
2023-05-08 14:01:32 +0400

    likse fixed

--- source/js/likes.js
+++ source/js/likes.js
@@ -1,7 +1,23 @@
 let cardLikes = document.querySelectorAll('.card-like');
+let cardValues = document.querySelectorAll('.card-like__value')
+let cardUses = document.querySelectorAll('.card-like__use')
 
-for (let cardLike of cardLikes) {
-  cardLike.onclick = function () {
-    cardLike.classList.toggle('card-like--added');
-  }
+let likeClick = function (cardLike, cardValue, cardUse) {
+  cardLike.addEventListener('click', function () {
+    let likeValue = Number(cardValue.textContent);
+    if (cardLike.classList.contains('card-like--added')) {
+      cardLike.classList.remove('card-like--added');
+      likeValue--;
+      cardUse.setAttribute('xlink:href', 'img/sprite.svg#like-icon');
+    } else {
+      cardLike.classList.add('card-like--added');
+      likeValue++;
+      cardUse.setAttribute('xlink:href', 'img/sprite.svg#like-icon-added');
+    }
+    cardValue.textContent = `${likeValue}`;
+  });
+}
+
+for (let i = 0; i < cardLikes.length; i++) {
+  likeClick(cardLikes[i], cardValues[i], cardUses[i]);
 }
